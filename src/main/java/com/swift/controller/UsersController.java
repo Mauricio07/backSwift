32e38e3d877d454db10c2fdb0e447aa19ed175bd
Mauricio <emauricioh7@gmail.com>
2023-06-22 16:53:59 -0500

    Correcciones en modelos

--- src/main/java/com/swift/controller/UsersController.java
+++ src/main/java/com/swift/controller/UsersController.java
@@ -1,11 +1,16 @@
 package com.swift.controller;
 
-import com.swift.services.exception.ResourceNotFoundException;
 import com.swift.model.Users;
 import com.swift.repository.UserReposiory;
+import com.swift.services.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
